1978d505a221b8117f84419934ed9a41f7f2a357
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-09-17 09:12:02 +0200

    Se creo el metodo "iniciarEditor" el cual llama al ProcessBuilder y con un parametro llamado nombre crea un fichero en la carpeta origen del proyecto y si ya está creado, lo inicializa.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Por favor introduce el nombre del fichero que quieres editar?: ");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        iniciarEditor(nombre);
+    }
+    public static void iniciarEditor(String nombre) {
+        try {
+            ProcessBuilder pBloc = new ProcessBuilder("gnome-text-editor",nombre);
+            Process p = pBloc.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
